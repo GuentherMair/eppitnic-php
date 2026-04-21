2ddef9c532924b7e422fc26fa1cb84feec872442
Günther Mair <guenther.mair@gmail.com>
2026-04-21 11:54:16 +0200

    dbupdate script added (increase length of various fields)

---
+++ docs/dbupdate/rev1011-tbl_contacts.sql
@@ -0,0 +1,4 @@
+ALTER TABLE tbl_contacts CHANGE COLUMN street street varchar(256);
+ALTER TABLE tbl_contacts CHANGE COLUMN street2 street2 varchar(128);
+ALTER TABLE tbl_contacts CHANGE COLUMN street3 street3 varchar(128);
+ALTER TABLE tbl_contacts CHANGE COLUMN city city varchar(128);
